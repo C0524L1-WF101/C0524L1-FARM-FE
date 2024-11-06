cf0c113358cc9e0a5bb088f9e71d7b10390e1a60
Tran Trung Kien <kientt1@vmogroup.com>
2024-11-06 21:23:12 +0700

    base reactjs

---
+++ farm-management/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="text-center mt-5">Hello, Bootstrap 5!</h1>
+      <button className="btn btn-primary">Click me</button>
+    </div>
+   
+  );
+}
+
+export default App;
