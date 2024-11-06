a84e0f05c3ca8b0d37d98952b01fa664c6a7eaf5
Tran Trung Kien <kientt1@vmogroup.com>
2024-11-06 21:42:20 +0700

    add component

--- farm-management/src/component/Header.js
+++ farm-management/src/component/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Home = () => {
+const Header = () => {
     return (
         <div className="container mt-4" >
-            Home
+            Header
         </div>
     );
 };
 
-export default Home;
+export default Header;
