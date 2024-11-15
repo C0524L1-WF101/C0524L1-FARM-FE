90afc0dbccdcfcaa6339fdbfd3f9515f3a8bd225
minleo1104 <dang50798@gmail.com>
2024-11-15 09:24:42 +0700

    fix edit profile page

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { AvatarProvider } from './AvatarContext'; // Import AvatarProvider
+
+ReactDOM.render(
+  <AvatarProvider>
     <App />
-  </React.StrictMode>
+  </AvatarProvider>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
