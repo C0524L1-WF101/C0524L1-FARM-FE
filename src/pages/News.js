242159f322e45cbb2bb8d0e4c07ac8386f5a5d0c
tung21112003 <tungnguyen2112003@gmail.com>
2024-11-19 16:02:57 +0700

    add-role

--- src/pages/News.js
+++ src/pages/News.js
@@ -12,6 +12,14 @@ const News = () => {
   const [notificationToDelete, setNotificationToDelete] = useState(null);
   const [action, setAction] = useState("");
   const [toast, setToast] = useState({ message: "", type: "", show: false });
+  const [role, setRole] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    const username = localStorage.getItem("username");
+    const userRole = localStorage.getItem("role");
+    setRole(userRole);
+    setIsLoggedIn(!!username);
+  }, []);
 
   const showToast = (message, type) => {
     setToast({ message, type, show: true });
@@ -118,22 +126,25 @@ const News = () => {
   return (
     <div className="news-home-container">
       <h1 className="news-home-header">Quản lý thông báo</h1>
-      <textarea
-        className="news-home-textarea"
-        placeholder="Nhập nội dung thông báo"
-        value={content}
-        onChange={(e) => setContent(e.target.value)}
-      />
-      <button
-        className="btn-add-notification"
-        onClick={() => {
-          setAction("add");
-          setShowAddModal(true);
-        }}
-      >
-        Thêm Thông báo mới
-      </button>
-
+      {role === "admin" && (
+        <textarea
+          className="news-home-textarea"
+          placeholder="Nhập nội dung thông báo"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
+      )}
+      {role === "admin" && (
+        <button
+          className="btn-add-notification"
+          onClick={() => {
+            setAction("add");
+            setShowAddModal(true);
+          }}
+        >
+          Thêm Thông báo mới
+        </button>
+      )}
       <div className="notification-list">
         {notifications.map((notification) => (
           <div key={notification.id} className="notification-item">
@@ -141,85 +152,106 @@ const News = () => {
             <small>
               Đăng vào: {new Date(notification.date).toLocaleString()}
             </small>
-            <button
-              className="btn-edit-notification"
-              onClick={() => {
-                setEditingId(notification.id);
-                setContent(notification.content);
-                setAction("edit");
-                setShowEditModal(true);
-              }}
-            >
-              Sửa
-            </button>
-            <button
-              className="btn-delete-notification"
-              onClick={() => {
-                setNotificationToDelete(notification.id);
-                setAction("delete");
-                setShowDeleteModal(true);
-              }}
-            >
-              Xóa
-            </button>
+            {role === "admin" && (
+              <button
+                className="btn-edit-notification"
+                onClick={() => {
+                  setEditingId(notification.id);
+                  setContent(notification.content);
+                  setAction("edit");
+                  setShowEditModal(true);
+                }}
+              >
+                Sửa
+              </button>
+            )}
+            {role === "admin" && (
+              <button
+                className="btn-delete-notification"
+                onClick={() => {
+                  setNotificationToDelete(notification.id);
+                  setAction("delete");
+                  setShowDeleteModal(true);
+                }}
+              >
+                Xóa
+              </button>
+            )}
           </div>
         ))}
       </div>
       <Modal show={showAddModal} onHide={() => setShowAddModal(false)}>
-  <Modal.Header className="modal-header-custom" closeButton>
-    <Modal.Title>Xác nhận Thêm Thông báo</Modal.Title>
-  </Modal.Header>
-  <Modal.Body className="modal-body-custom">
-    Bạn có chắc chắn muốn thêm thông báo này không?
-  </Modal.Body>
-  <Modal.Footer className="modal-footer-custom">
-    <Button className="button-secondary" onClick={() => setShowAddModal(false)}>
-      Hủy
-    </Button>
-    <Button className="button-primary" onClick={() => handleModalConfirm("add")}>
-      Thêm Thông báo
-    </Button>
-  </Modal.Footer>
-</Modal>
+        <Modal.Header className="modal-header-custom" closeButton>
+          <Modal.Title>Xác nhận Thêm Thông báo</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="modal-body-custom">
+          Bạn có chắc chắn muốn thêm thông báo này không?
+        </Modal.Body>
+        <Modal.Footer className="modal-footer-custom">
+          <Button
+            className="button-secondary"
+            onClick={() => setShowAddModal(false)}
+          >
+            Hủy
+          </Button>
+          <Button
+            className="button-primary"
+            onClick={() => handleModalConfirm("add")}
+          >
+            Thêm Thông báo
+          </Button>
+        </Modal.Footer>
+      </Modal>
 
-<Modal show={showEditModal} onHide={() => setShowEditModal(false)}>
-  <Modal.Header className="modal-header-custom" closeButton>
-    <Modal.Title>Xác nhận chỉnh sửa thông báo</Modal.Title>
-  </Modal.Header>
-  <Modal.Body className="modal-body-custom">
-    <textarea
-      className="news-home-textarea"
-      value={content}
-      onChange={(e) => setContent(e.target.value)}
-    />
-  </Modal.Body>
-  <Modal.Footer className="modal-footer-custom">
-    <Button className="button-secondary" onClick={() => setShowEditModal(false)}>
-      Hủy
-    </Button>
-    <Button className="button-primary" onClick={() => handleModalConfirm("edit")}>
-      Cập Nhật Thông Báo
-    </Button>
-  </Modal.Footer>
-</Modal>
+      <Modal show={showEditModal} onHide={() => setShowEditModal(false)}>
+        <Modal.Header className="modal-header-custom" closeButton>
+          <Modal.Title>Xác nhận chỉnh sửa thông báo</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="modal-body-custom">
+          <textarea
+            className="news-home-textarea"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+        </Modal.Body>
+        <Modal.Footer className="modal-footer-custom">
+          <Button
+            className="button-secondary"
+            onClick={() => setShowEditModal(false)}
+          >
+            Hủy
+          </Button>
+          <Button
+            className="button-primary"
+            onClick={() => handleModalConfirm("edit")}
+          >
+            Cập Nhật Thông Báo
+          </Button>
+        </Modal.Footer>
+      </Modal>
 
-<Modal show={showDeleteModal} onHide={() => setShowDeleteModal(false)}>
-  <Modal.Header className="modal-header-custom" closeButton>
-    <Modal.Title>Xác nhận xóa thông báo</Modal.Title>
-  </Modal.Header>
-  <Modal.Body className="modal-body-custom">
-    Bạn có chắc chắn muốn xóa thông báo này không?
-  </Modal.Body>
-  <Modal.Footer className="modal-footer-custom">
-    <Button className="button-secondary" onClick={() => setShowDeleteModal(false)}>
-      Hủy
-    </Button>
-    <Button className="button-danger" onClick={() => handleModalConfirm("delete")}>
-      Xóa Thông Báo
-    </Button>
-  </Modal.Footer>
-</Modal>
-
+      <Modal show={showDeleteModal} onHide={() => setShowDeleteModal(false)}>
+        <Modal.Header className="modal-header-custom" closeButton>
+          <Modal.Title>Xác nhận xóa thông báo</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="modal-body-custom">
+          Bạn có chắc chắn muốn xóa thông báo này không?
+        </Modal.Body>
+        <Modal.Footer className="modal-footer-custom">
+          <Button
+            className="button-secondary"
+            onClick={() => setShowDeleteModal(false)}
+          >
+            Hủy
+          </Button>
+          <Button
+            className="button-danger"
+            onClick={() => handleModalConfirm("delete")}
+          >
+            Xóa Thông Báo
+          </Button>
+        </Modal.Footer>
+      </Modal>
 
       <ToastNotification
         message={toast.message}
